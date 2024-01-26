f1eacb59966b4d4e14495b42ab34454ed17f8e49
Saurav7185 <101385475+Saurav7185@users.noreply.github.com>
2024-01-26 09:30:16 +0530

    PROJECT

---
+++ BEE WEBSITE/client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyClfKrAUMOXjeGmvZEnvF2dULN6e5m2hdk',
+  authDomain: "real-estate-36349.firebaseapp.com",
+  projectId: "real-estate-36349",
+  storageBucket: "real-estate-36349.appspot.com",
+  messagingSenderId: "138720488273",
+  appId: "1:138720488273:web:70ff7795bb24ec70e94170"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
